a8993d159dd3e249595decf4afac6efa0aad2910
mdlugoszewski <marcindlu993@gmail.com>
2018-02-02 08:57:13 +0100

    Add project files.

---
+++ APIPio/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIPio.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ APIPio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIPio.WebApiApplication" Language="C#" %>
